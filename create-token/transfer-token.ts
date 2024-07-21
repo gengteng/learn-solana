2f9636b0c4c94f28b748be8826430e38207f74bd
gengteng <me@gteng.org>
2024-07-21 18:39:27 +0800

    feat: transfer token

---
+++ create-token/transfer-token.ts
@@ -0,0 +1,59 @@
+import "dotenv/config";
+import {
+  getExplorerLink,
+  getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import { HttpsProxyAgent } from "hpagent";
+
+const httpAgent = new HttpsProxyAgent({ proxy: "http://127.0.0.1:7890" });
+const connection = new Connection(clusterApiUrl("devnet"), {httpAgent, commitment: "confirmed"});
+
+
+const sender = getKeypairFromEnvironment("A_SECRET_KEY");
+const receiver = getKeypairFromEnvironment("B_SECRET_KEY");
+
+console.log(
+  `🔑 Loaded our keypair securely, using an env file! Our public key is: ${sender.publicKey.toBase58()}`
+);
+
+// Add the recipient public key here.
+const recipient = receiver.publicKey;
+
+// Subtitute in your token mint account
+const tokenMintAccount = new PublicKey("BNDqUZkZqeV6nmreNtPsYtgo9RCcuxKERbqFEx7c25tL");
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+console.log(`💸 Attempting to send 1 token to ${recipient.toBase58()}...`);
+
+// Get or create the source and destination token accounts to store this token
+const sourceTokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  sender,
+  tokenMintAccount,
+  sender.publicKey
+);
+
+const destinationTokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  sender,
+  tokenMintAccount,
+  recipient
+);
+
+// Transfer the tokens
+const signature = await transfer(
+  connection,
+  sender,
+  sourceTokenAccount.address,
+  destinationTokenAccount.address,
+  sender,
+  1 * MINOR_UNITS_PER_MAJOR_UNITS
+);
+
+const explorerLink = getExplorerLink("transaction", signature, "devnet");
+
+console.log(`✅ Transaction confirmed, explorer link is: ${explorerLink}!`);
